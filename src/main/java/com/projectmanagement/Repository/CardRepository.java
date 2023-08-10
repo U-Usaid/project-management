299099e6f7c83e6c9fe63611e72344f77684f0e8
U-Usaid <osaid.osaid66@gmail.com>
2023-08-10 11:52:58 +0400

    adding queries

--- src/main/java/com/projectmanagement/Repository/CardRepository.java
+++ src/main/java/com/projectmanagement/Repository/CardRepository.java
@@ -4,6 +4,10 @@ import com.projectmanagement.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardRepository extends JpaRepository <Card, Integer> {
+    List<Card> findBoardId(Integer boardId);
+    Card findBoardCardId(Integer boardId, Integer cardId);
 }
