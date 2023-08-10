b806ce0f932af5198599edaf2d6454f69d1c447e
U-Usaid <osaid.osaid66@gmail.com>
2023-08-10 11:48:32 +0400

    Major updating adding many to one

--- src/main/java/com/projectmanagement/model/Card.java
+++ src/main/java/com/projectmanagement/model/Card.java
@@ -1,15 +1,38 @@
 package com.projectmanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cards")
 public class Card {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     public Integer id;
     public String title;
+
+    @Column(length = 1000)
     public String description;
+
+    @Column(nullable = false)
     public int section;
+
+
+    @ManyToOne
+    @JoinColumn(name = "BoardId", nullable = false)
+    public Board board;
+
+
+    public String getSectionName(){
+        return Board.getSectionName(this.section);
+    }
 
 }
